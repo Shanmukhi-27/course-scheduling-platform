98dd3038ea385ea9cd9681a603ed6aec7517106c
Shanmukhi-27 <2400080230@kluniversity.in>
2026-05-01 22:30:03 +0530

    Remove backend folder from repo

--- backend/models/Registration.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize')
-const sequelize = require('../config/db')
-
-const Registration = sequelize.define('Registration', {
-  studentId: { type: DataTypes.STRING, allowNull: false },
-  courseId: { type: DataTypes.INTEGER, allowNull: false }
-})
-
-module.exports = Registration
